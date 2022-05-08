8dea3b601573d18e69e927eb7e1403f4637dcbe9
mitanvir05 <tanvir.cse05@gmail.com>
2022-05-08 15:43:16 +0600

    update and delivery added

--- src/Pages/InventoryDetail/InventoryDetail.js
+++ src/Pages/InventoryDetail/InventoryDetail.js
@@ -4,12 +4,65 @@ import { useParams } from "react-router-dom";
 const InventoryDetail = () => {
   const { _id } = useParams();
   const [inventoriesDetail, setInventoriesDetail] = useState({});
+  let url = `http://localhost:5000/inventory/${_id}`;
   useEffect(() => {
-    const url = `http://localhost:5000/inventory/${_id}`;
+    let url = `http://localhost:5000/inventory/${_id}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => setInventoriesDetail(data));
-  }, [_id]);
+  }, [_id, inventoriesDetail]);
+
+  const handleDelivered = () => {
+    let quantity = inventoriesDetail?.quantity;
+    quantity = parseInt(quantity) - 1;
+
+    if (quantity < 0) {
+      return alert("Invalid quantity");
+    }
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ quantity }),
+    });
+  };
+
+  const handleQuantity = (e) => {
+    e.preventDefault();
+
+    let quantity = inventoriesDetail?.quantity;
+    console.log(e.target);
+    const addQuantity = parseInt(e?.target?.quantity?.value);
+    console.log(addQuantity);
+
+    if (addQuantity > 0) {
+      quantity = parseInt(quantity) + addQuantity;
+      const updatedInventory = { quantity };
+
+      const url = `http://localhost:5000/inventory/${_id}`;
+
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(updatedInventory),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          alert("quantity updated successfully");
+          e?.target?.reset();
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("Please insert positive number of quantity");
+      e?.target?.reset();
+      return;
+    }
+  };
+
   return (
     <div className="g-5 col-sm-12 col-md-6 col-lg-4">
       <div className="card ">
@@ -20,13 +73,25 @@ const InventoryDetail = () => {
           <h6> Quantity : {inventoriesDetail.quantity}</h6>
           <h6>Supplier : {inventoriesDetail.supplier}</h6>
           <p className="card-text">{inventoriesDetail.description}</p>
-          <button className="btn btn-primary">Delivered</button>
+          <button onClick={handleDelivered} className="btn btn-primary">
+            Delivered
+          </button>
         </div>
       </div>
 
       <div>
-        <input className="m-2" placeholder="Enter Quantity" type="number" />
-        <button className="btn btn-primary">Add Quantity</button>
+        <form onSubmit={handleQuantity}>
+          <input
+            className="m-2"
+            placeholder="Enter Quantity"
+            name="quantity"
+            type="text"
+            required
+          />
+          <button className="btn btn-primary" type="submit">
+            Add Quantity
+          </button>
+        </form>
       </div>
     </div>
   );
